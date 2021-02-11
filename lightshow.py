7ed6dd1c9f818e48c62019d62c8363fc50562702
blacktack2 <stuart.manfred.lewis@gmail.com>
2021-02-11 14:15:05 +0000

    Moved lightshow_backup.py contents to lightshow.py

--- lightshow.py
+++ lightshow.py
@@ -1,35 +1,9 @@
 import math
-from pathlib import Path
-from PIL import ImageFont
 from rpi_ws281x import *
 import socket
 import sys
 import time
-import threading
 import traceback
-
-try:
-    from Queue import Queue
-except ImportError:
-    from queue import Queue
-
-from demo_opts import get_device
-from luma.core.render import canvas
-from luma.core.virtual import viewport
-
-try:
-    import tweepy
-except ImportError:
-    print("The tweepy library was not found. Run 'sudo -H pip install tweepy' to install it.")
-    sys.exit()
-
-
-consumer_key = "TWITTER_API_CONSUMER_KEY"
-consumer_secret = "TWITTER_API_CONSUMER_SECRET"
-access_token = "TWITTER_API_ACCESS_TOKEN"
-access_token_secret = "TWITTER_API_ACCESS_TOKEN_SECRET"
-
-search_terms = ['python']
 
 
 LED_COUNT      = 11
@@ -43,49 +17,7 @@ LED_INVERT     = False
 # BUTTON_CHANNEL = 19
 
 
-# MONITOR FUNCTIONS
-class Listener(tweepy.StreamListener):
-    def __init__(self, queue):
-        super(Listener, self).__init__()
-        self.queue = queue
-
-    def on_status(self, status):
-        self.queue.put(status)
-
-
-def message_scroll_thread(queue):
-    while True:
-        status = queue.get()
-        scroll_message(status, font=font)
-
-
-def make_font(name, size):
-    font_path = str(Path(__file__).resolve().parent.joinpath('fonts', name))
-    return ImageFont.truetype(font_path, size)
-
-
-def scroll_message(status, speed=1):
-    author = u"@{0}".format(status.author.screen_name)
-    full_text = u"{0}  {1}".format(author, status.text).replace("\n", " ")
-    x = device.width
-
-    with canvas(device) as draw:
-        w, h = draw.textsize(full_text, font)
-
-    virtual = viewport(device, width=max(device.width, w + x + x), height=max(h, device.height))
-    with canvas(virtual) as draw:
-        draw.text((x, 0), full_text, font=font, fill="white")
-        draw.text((x, 0), author, font=font, fill="yellow")
-
-    i = 0
-    while i < x + w:
-        virtual.set_position((i, 0))
-        i += speed
-        time.sleep(0.025)
-
-
-# STRIP FUNCTIONS
-def init_strip():
+def init():
     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
     colors = []
     strip.begin()
@@ -124,26 +56,19 @@ def set_all_pixels(strip, colors, c):
 
 
 def main():
-    # Hosting
     host = ""
-    port = 8000
+    port = 8001
     backlog = 5
-    # Display
-    device = get_device()
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-    queue = Queue()
-    font = make_font("code2000.ttf", 12)
-    # Strip
     size = 1024
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((host, port))
     sock.listen(backlog)
-    strip, colors = init_strip()
+    strip, colors = init()
+    print("Waiting for connections")
     while True:
         client, address = sock.accept()
         client.sendall(b"Client connected")
+        print("Client connected")
         try:
             while True:
                 client.sendall(b"\n> ")
@@ -159,7 +84,15 @@ def main():
                 if data == b"exit":
                     client.sendall(b"Client asked server to quit\n")
                     client.send(data)
+                    client.close()
+                    clear_strip(strip, colors)
                     return
+                if data == b"help":
+                    client.sendall(b"disconnect\n")
+                    client.sendall(b"exit\n")
+                    client.sendall(b"clear\n")
+                    client.sendall(b"set index r g b\n")
+                    client.sendall(b"setall r g b\n")
                 if data == b"clear":
                     client.sendall(b"Clearing strip")
                     clear_strip(strip, colors)
@@ -179,32 +112,17 @@ def main():
                         g = int(data_tokens[2])
                         b = int(data_tokens[3])
                         set_all_pixels(strip, colors, Color(r, g, b))
-                    if data_tokens[0] == b"say":
-                        text = data_tokens[1]
-                        stream = tweepy.Stream(auth=api.auth, listener=Listener(queue))
-                        scroll_message(queue.get(), font=font)
-                        #message_thread = threading.Thread(target=message_scroll_thread, args=(queue,))
-                        #message_thread.start()
                 except Exception as err:
                     client.sendall(b"Error occured parsing input\n")
                     traceback.print_exc()
         except Exception as err:
             client.sendall(b"Error occured, Server shutting down\n")
             traceback.print_exc()
-        finally:
             print("Closing server")
             client.close()
             clear_strip(strip, colors)
-            if message_thread:
-                message_thread.join()
             return
 
 
-# Globals
-device = None
-font = None
-message_thread = None
-
-# Run
 main()
 
