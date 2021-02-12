7fe3835c71dcce9fe833c2f0c5f95e689a66dbc3
blacktack2 <stuart.manfred.lewis@gmail.com>
2021-02-12 12:50:33 +0000

    Combined display.py into lightshow.py

--- lightshow.py
+++ lightshow.py
@@ -4,6 +4,8 @@
 # Version 1.0 - 10-02-2021 [Basic implementation for setting pixels remotely]
 # Version 1.1 - 11-02-2021 [Addition of aliases and some small QOL improvements]
 
+from __future__ import unicode_literals
+import threading
 from typing import *
 import os
 import math
@@ -13,17 +15,17 @@ import sys
 import time
 import traceback
 import json
+from pathlib import Path
+from PIL import ImageFont
 
+try:
+    from Queue import Queue
+except ImportError:
+    from queue import Queue
 
-LED_COUNT      = 11
-LED_PIN        = 18
-LED_FREQ_HZ    = 800000
-LED_DMA        = 10
-LED_BRIGHTNESS = 255
-LED_INVERT     = False
-# LED_CHANNEL    = 0
-
-# BUTTON_CHANNEL = 19
+from demo_opts import get_device
+from luma.core.render import canvas
+from luma.core.virtual import viewport
 
 
 class InputError(Exception):
@@ -31,15 +33,69 @@ class InputError(Exception):
         super().__init__("Input Exception")
 
 
-def init():
-    global strip, colors
-    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
-    colors = []
-    strip.begin()
+class ScrollThread(threading.Thread):
+    def __init__(self):
+        super().__init__()
 
-    for i in range(0, strip.numPixels(), 1):
-        colors.append(Color(0, 0, 0))
-    update_strip()
+    def run(self):
+        while do_thread:
+            waiting_thread = WaitThread()
+            waiting_thread.start()
+            new_text = queue.get()
+            waiting_thread.stop()
+            waiting_thread.join()
+            if new_text is not None:
+                self.scroll_message(new_text, font, message_speed)
+
+    def scroll_message(self, text, font, speed):
+        with canvas(device) as draw:
+            w, h = draw.textsize(text, font)
+        x = device.width
+        y = device.height // 2 - h // 2
+
+        virtual = viewport(device, width=max(device.width, w + x + x), height=max(device.height, h))
+
+        with canvas(virtual) as draw:
+            draw.text((x, y), text, font=font, fill="white")
+
+        i = 0
+        while i < x + w and do_thread:
+            virtual.set_position((i, 0))
+            i += speed
+            time.sleep(0.025)
+
+        with canvas(device) as draw:
+            draw.rectangle(device.bounding_box, fill="black")
+
+
+class WaitThread(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.running = True
+
+    def run(self):
+        global font_changed
+        counter = 0
+        with canvas(device) as draw:
+            w, h = draw.textsize("Waiting...", font)
+        while self.running:
+            if font_changed:
+                font_changed = False
+                with canvas(device) as draw:
+                    w, h = draw.textsize("Waiting...", font)
+            text = "Waiting" + "." * counter
+            with canvas(device) as draw:
+                draw.text((device.width // 2 - w // 2, device.height // 2 - h // 2), text, font=font, fill="white")
+            counter = (counter + 1) % 4
+            time.sleep(0.2)
+
+    def stop(self):
+        self.running = False
+
+
+def make_font(name, size):
+    font_path = str(Path(__file__).resolve().parent.joinpath('fonts', name))
+    return ImageFont.truetype(font_path, size)
 
 
 def update_strip():
@@ -155,14 +211,23 @@ def get_led(alias: str) -> int:
 
 
 def main():
-    global client, strip, cmd_fail_message, colors
+    global client, strip, cmd_fail_message, colors, scroll_thread
     print("Setting up...")
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((host, port))
     sock.listen(backlog)
+
+    print("Initialising threads...")
+    scroll_thread = ScrollThread()
+    scroll_thread.start()
+
     print("Initialising strip...")
-    init()
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+    colors = [Color(0, 0, 0) for _ in range(strip.numPixels())]
+    strip.begin()
+    update_strip()
+
     print("Waiting for connections")
     try:
         while True:
@@ -185,8 +250,11 @@ def main():
                         clear_strip()
                         return
                     if data == "help":
+                        client.sendall(b"-System Commands-\n")
                         client.sendall(b"disconnect\n")
                         client.sendall(b"exit\n")
+                        client.sendall(b"help\n")
+                        client.sendall(b"-Strip Commands-\n")
                         client.sendall(b"clear-pixels\n")
                         client.sendall(b"set <pixel> <r> <g> <b>\n")
                         client.sendall(b"setall <r> <g> <b>\n")
@@ -196,6 +264,10 @@ def main():
                         client.sendall(b"list-alias-stores\n")
                         client.sendall(b"list-aliases\n")
                         client.sendall(b"clear-aliases\n")
+                        client.sendall(b"-Display Commands-\n")
+                        client.sendall(b"say <message>\n")
+                        client.sendall(b"message-speed <speed>\n")
+                        client.sendall(b"font-size <size>\n")
                         continue
                     if data == "clear-pixels":
                         client.sendall(b"Clearing strip\n")
@@ -249,6 +321,27 @@ def main():
                             load_aliases(name)
                             client.sendall(bytes("Loaded aliases from " + name + "\n", "utf-8"))
                             continue
+
+                        # say <message>
+                        if data_tokens[0] == "say":
+                            message = " ".join(data_tokens[1:])
+                            queue.put(message)
+                            client.sendall(bytes("Added message '" + message + "' to queue\n", "utf-8"))
+                            continue
+                        # message-speed <speed>
+                        if data_tokens[0] == "message-speed":
+                            global message_speed
+                            message_speed = int(data_tokens[1])
+                            client.sendall(bytes("Set message speed to " + data_tokens[1] + "\n", "utf-8"))
+                            continue
+                        # font-size <size>
+                        if data_tokens[0] == "font-size":
+                            global font, font_changed
+                            fsize = int(data_tokens[1])
+                            font = make_font(u"code2000.ttf", fsize)
+                            font_changed = True
+                            client.sendall(bytes("Set font size to " + data_tokens[1] + "\n", "utf-8"))
+                            continue
                         cmd_fail_message = "Command " + data_tokens[0] + " not recognised."
                         raise InputError()
                     except InputError as err:
@@ -263,21 +356,44 @@ def main():
                 traceback.print_exc()
                 return
     finally:
+        global do_thread
         print("Closing server")
+        do_thread = False
+        with queue.mutex:
+            queue.queue.clear()
+        queue.put(None)
         clear_strip()
         sock.close()
 
+
+
+# CONSTANTS
+LED_COUNT      = 11
+LED_PIN        = 18
+LED_FREQ_HZ    = 800000
+LED_DMA        = 10
+LED_BRIGHTNESS = 255
+LED_INVERT     = False
 
 # GLOBAL VARIABLES
 cmd_fail_message = None
 aliases = {}
 strip = None
+colors = None
+
+font_changed = False
+do_thread = True
+
 client = None
 host = ""
 port = 8001
 backlog = 5
 size = 1024
-colors = None
+
+device = get_device()
+queue = Queue()
+font = make_font(u"code2000.ttf", 25)
+message_speed = 10
 
 # RUN
 main()
